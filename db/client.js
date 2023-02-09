eb5b28ee3d354508432d6558c16a404dea2a62f0
UrrutiaGeo <geovannyurrutia@gmail.com>
2023-02-09 15:22:43 -0600

    Load dotenv

--- db/client.js
+++ db/client.js
@@ -1,12 +1,14 @@
+import * as dotenv from 'dotenv'
 import pg from 'pg';
+dotenv.config();
 
 const {Client} = pg;
 
 const connectionData = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: `${process.env.DB_USER}`,
+    host: `${process.env.DB_HOST}`,
+    database: `${process.env.DB_NAME}`,
+    password: `${process.env.DB_PASSWORD}`,
     port: process.env.DB_PORT,
 };
 
